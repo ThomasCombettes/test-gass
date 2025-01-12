2af85f301ea02487dfcb49ea4b5440079b0b93f3
gatien sicot <GassdisleG>
2025-01-12 21:02:19 +0100

    début exercice

--- exercice/Calculette.c
+++ exercice/Calculette.c
@@ -4,7 +4,7 @@
 int main()
 
 {
-  double resultat = 0, nombre1 = 0, nombre2 = 0;
+  double resultat = 0, nombre1 = 0, nombre2 = 0, nombre3 = 0;
   
   // On demande les nombres 1 et 2 à l'utilisateur :
   
@@ -12,14 +12,16 @@ int main()
   scanf("%lf", &nombre1);
   printf("Entrez le nombre 2 : ");
   scanf("%lf", &nombre2);
+  printf("Entrez le nombre 3 : ");
+  scanf("%lf", &nombre3);
   
   // On fait le calcul :
   
-  resultat = nombre1 + nombre2;
+  resultat = nombre1 + nombre2 + nombre3;
   
   // Et on affiche l'addition à l'écran :
   
-  printf ("%f + %f = %f\n", nombre1, nombre2, resultat);
+  printf ("%f + %f = %f\n", nombre1, nombre2, nombre3 ,resultat);
        
   return 0;
 }
